6c3fa81068ba73cdbc3e7571dbfdd56b62d4a32d
CarsonBain <carsonbain@gmail.com>
2018-03-24 14:39:56 -0700

    Initial Commit
    Adding all files to repo

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function(){
+  //mobile menu functionality
+$('.mobile-menu ul').css('display', 'none');
+$('.hamburger__wrap').click(function(){
+  $('.mobile-menu ul').slideToggle();
+  $('.hamburger-menu').toggleClass('open');
+});
+
+$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+});
+
+});
